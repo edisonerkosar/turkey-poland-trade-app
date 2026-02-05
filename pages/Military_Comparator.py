439c2a097940d37576f1629e418231d7be4c4262
edisonerkosar <edisonerkosar@gmail.com>
2026-02-05 15:14:38 +0100

    Update Military_Comparator.py

--- pages/Military_Comparator.py
+++ pages/Military_Comparator.py
@@ -242,7 +242,7 @@ if view_mode == "Home (EU Comparison)":
 
     cagr_df = pd.DataFrame(cagr_rows)
 
-        if not cagr_df.empty:
+    if not cagr_df.empty:
             cagr_df = cagr_df.sort_values("CAGR", ascending=False)
 
             fig_cagr = px.bar(
